9ba86bc6956019b8403ae17640ff43cfa74f0197
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-11 15:50:19 +0300

    added app url routes

--- legram/urls.py
+++ legram/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('', views.home, name = 'gram-home'),
+    url(r'^$', views.home, name = 'gram-home'),
+    url(r'profile/', views.profile, name = 'gram-profile'),
+    url(r'profile/update_profile/', views.update_profile, name = 'gram-profile-update'),
+    url(r'new_post/', views.new_image_post, name = 'gram-post'),
 ]
