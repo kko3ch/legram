8fee6c85fbe07b1251a8813671dc350de466edbf
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-11 15:49:16 +0300

    added models to admin

--- legram/admin.py
+++ legram/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import Profile,Image,Comment,Follower,Following
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Follower)
+admin.site.register(Following)
+
+
