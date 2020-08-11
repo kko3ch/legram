b200c5a3e1b109041f390158f006a4ed2aa038c5
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-11 15:48:55 +0300

    added project urls

--- gram/urls.py
+++ gram/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('legram.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
+    url('tinymce/', include('tinymce.urls')),
 ]
