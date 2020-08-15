060812e1e55a59d4d16adcc368beccd25e3a8662
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-15 13:09:13 +0300

    added comment form

--- legram/forms.py
+++ legram/forms.py
@@ -1,4 +1,4 @@
-from . models import Image,Profile
+from . models import Image,Profile,Comment
 from django import forms
 
 class NewImageForm(forms.ModelForm):
@@ -10,3 +10,12 @@ class EditProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['profile','followers','following']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['image','name','pub_date']
+        widgets = {
+            'body': forms.Textarea(attrs={'class':'form-control','required': 'false'})
+        }
